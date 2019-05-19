eefecf9ac0e0c540d732ea82382ebabee1afec6c
Francisco Prior <franciscoj.prior@hotmail,com>
2019-05-18 23:03:52 -0500

    Add readTrips code.

---
+++ Itinerary/Model/Data.swift
@@ -0,0 +1,15 @@
+//
+//  Data.swift
+//  Itinerary
+//
+//  Created by P on 5/18/19.
+//  Copyright © 2019 Francisco Prior. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    static var tripModels = [TripModel]()
+    
+    
+}
