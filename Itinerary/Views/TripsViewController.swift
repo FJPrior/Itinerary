0b5411cbc51f45be81c9a2d51f3ea444e4b0a5cf
Francisco Prior <franciscoj.prior@hotmail,com>
2019-05-20 17:47:18 -0500

    Run data retrieval on background thread.

---
+++ Itinerary/Views/TripsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TripsViewController.swift
+//  Itinerary
+//
+//  Created by P on 5/20/19.
+//  Copyright © 2019 Francisco Prior. All rights reserved.
+//
+
+import UIKit
+
+class TripsViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    
+}
